02d4124a40a464ae73261f035142963213ae0f31
alina-str <al-strukova@mail.ru>
2021-04-09 21:09:43 +0300

    add: task 4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,47 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const a = [];
+  for (let i = 0; i < matrix.length; i++) {
+    a[i] = [];
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      a[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let k = 0;
+      if (i !== matrix.length - 1) {
+        if (matrix[i + 1][j] === true) k++;
+      }
+      if ((i !== matrix.length - 1) && (j !== 0)) {
+        if (matrix[i + 1][j - 1] === true) k++;
+      }
+      if (j !== 0) {
+        if (matrix[i][j - 1] === true) k++;
+      }
+      if ((j !== 0) && (i !== 0)) {
+        if (matrix[i - 1][j - 1] === true) k++;
+      }
+      if (i !== 0) {
+        if (matrix[i - 1][j] === true) k++;
+      }
+      if ((j !== matrix[i].length - 1) && (i !== 0)) {
+        if (matrix[i - 1][j + 1] === true) k++;
+      }
+      if (j !== matrix[i].length - 1) {
+        if (matrix[i][j + 1] === true) k++;
+      }
+      if ((i !== matrix.length - 1) && (j !== matrix[i].length - 1)) {
+        if (matrix[i + 1][j + 1] === true) k++;
+      }
+      a[i][j] = k;
+    }
+  }
+  return a;
 }
 
 module.exports = minesweeper;
