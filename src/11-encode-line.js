33810180a9c351ba6f09f406fba7bab3cfd6b2dc
alina-str <al-strukova@mail.ru>
2021-04-10 13:24:47 +0300

    add: task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let s = '';
+  let j = 0;
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    let k = 0;
+    j = i;
+    while (str[j] === char) {
+      k++;
+      j++;
+      i++;
+    }
+    if (k !== 1) s += k.toString() + char;
+    else s += char;
+    i--;
+  }
+  return s;
 }
 
 module.exports = encodeLine;
