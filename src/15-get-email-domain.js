7f0ea240011de828e610185e603caa107925710c
alina-str <al-strukova@mail.ru>
2021-04-11 12:24:51 +0300

    add: task 15

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,19 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  let s = '';
+  let k = 0;
+  for (let i = email.length; i > -1; i--) {
+    if (email[i] === '@') {
+      k = i;
+      break;
+    }
+  }
+  for (let j = k + 1; j < email.length; j++) {
+    s += email[j];
+  }
+  return s;
 }
 
 module.exports = getEmailDomain;
